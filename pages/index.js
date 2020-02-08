3352d663ab391eacba3b2e232ab13d1d4d58914d
Ryan Irilli <ryanirilli@gmail.com>
2020-02-08 09:26:57 -0800

    add intro

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Head from "next/head";
 import { E } from "../design-system";
 import { useInView } from "react-intersection-observer";
+import lottie from "lottie-web";
+import budchellaIntro from "../budchella-intro.json";
 
 const Container = React.forwardRef((p, ref) => <E {...p} ref={ref} />);
 const Content = React.forwardRef((p, ref) => <E {...p} ref={ref} />);
-const THRESHOLD = [0.5];
-const Home = () => {
-  const [ref, inView] = useInView({
-    threshold: THRESHOLD,
-    triggerOnce: true
-  });
 
+const intObsOpts = {
+  threshold: [0.5],
+  triggerOnce: true
+};
+
+const Home = () => {
+  const introRef = useRef();
+  const [sectionOneRef, isSectionOneInView] = useInView(intObsOpts);
+  const [sectionTwoRef, isSectionTwoInView] = useInView(intObsOpts);
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: introRef.current,
+      renderer: "svg",
+      loop: false,
+      autoplay: true,
+      animationData: budchellaIntro
+    });
+  }, []);
   return (
     <>
       <Head>
         <title>Budchella Music and Arts Frestival</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container fullPage centerContent bgImage="./background.jpg">
-        <Content m={{ paddingSides: "m" }} l={{ paddingSides: "l" }}></Content>
+      <Container fullPage centerContent>
+        <Content>
+          <div ref={introRef} />
+        </Content>
       </Container>
-      <Container ref={ref} fullPage bgColor={inView ? "blue" : "yellow"} />
-      <Container fullPage bgColor="red" />
+      <Container ref={sectionOneRef} fullPage />
+      <Container ref={sectionTwoRef} fullPage />
     </>
   );
 };
